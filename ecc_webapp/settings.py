3714eb746d0fa16b624306b358ab7de8b6eb4c7b
bosirullah <mdbosirullah@gmail.com>
2024-01-05 22:47:15 +0530

    deployment bug fixes - 5

--- ecc_webapp/settings.py
+++ ecc_webapp/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ecc',
-    'django_bootstrap5',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
